80392b612581dfdaa9a46c3deed6c39c9561b295
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-14 15:45:35 -0500

    Create openstack_ctl_logrotate.sh

---
+++ Platforms/Mirantis/PaaS/MCP/Scripts/Administration/Logrotate/openstack_ctl_logrotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# =====================================================
+# Author: Richard Barrett
+# Date Created: 03/14/2020
+# Organziation: Mirantis
+# Purpose: Initalize Manual Logrotate on Openstack Logs
+# =====================================================
+
+# Rotate the following logs from salt master
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/keystone"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/cinder-common"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/glance-common"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/nova-common"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/neutron-common"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/heat-common"
+sudo salt '*ctl*' cmd.run "sudo logrotate -f /etc/logrotate.d/designate-common"
