ff03db244db19054243969eab3371e9fdc8ee112
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-06-26 14:02:38 -0500

    Update CleanUp.ps1

--- Administration/Windows/CleanUp.ps1
+++ Administration/Windows/CleanUp.ps1
@@ -3,4 +3,35 @@
 
 cd ~
 #Remove-Item –path ~\.\Git\ -Recurse -Filter *test* -whatif
+Remove-Item –path ~\.\.aws\ -Recurse
+Remove-Item –path ~\.\.bash_*\ -Recurse
+Remove-Item –path ~\.\.conda*\ -Recurse
+Remove-Item –path ~\.\.python_history\ -Recurse
+Remove-Item –path ~\.\.conda*\ -Recurse
+Remove-Item –path ~\.\.ssh\ -Recurse
+Remove-Item –path ~\.\Clouds\ -Recurse
 Remove-Item –path ~\.\Git\ -Recurse
+Remove-Item –path ~\.\Keys\ -Recurse
+Remove-Item –path ~\.\Kubernetes\ -Recurse
+Remove-Item –path ~\.\Personal\ -Recurse
+Remove-Item –path ~\.\Projects\ -Recurse
+Remove-Item –path ~\.\knime-workspace\ -Recurse
+Remove-Item –path ~\.\OpenVPN\ -Recurse
+Remove-Item –path ~\.\PycharmProjects\ -Recurse
+Remove-Item –path ~\.\Postman\ -Recurse
+Remove-Item –path ~\.\Scripts\ -Recurse
+
+cd ~\.\Documents\
+  Remove-Item * -Recurse
+cd ~\.\Downloads\
+  Remove-Item * -Recurse
+cd ~\.\Images\
+  Remove-Item * -Recurse
+cd ~\.\Photos\
+  Remove-Item * -Recurse
+cd ~\.\Pictures\
+  Remove-Item * -Recurse
+cd '~\VirtualBox VMs\'
+  Remove-Item * -Recurse
+  
+
