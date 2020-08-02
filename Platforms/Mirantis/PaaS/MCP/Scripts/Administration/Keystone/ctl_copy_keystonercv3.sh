b0305d991cbcf5f1511d2745d3aac67416f7f5e0
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-08-01 19:54:03 -0500

    Update ctl_copy_keystonercv3.sh

--- Platforms/Mirantis/PaaS/MCP/Scripts/Administration/Keystone/ctl_copy_keystonercv3.sh
+++ Platforms/Mirantis/PaaS/MCP/Scripts/Administration/Keystone/ctl_copy_keystonercv3.sh
@@ -2,7 +2,7 @@
 
 # Copy the Keystone File over
 printf "Copying Keystonercv3 file over to your home directory...\n"
-sudo salt "*ctl*" cmd.run "sudo cp /root/keystonercv3 /home/rbarrett"
+sudo salt "*ctl*" cmd.run "sudo cp /root/keystonercv3 /home/$USER"
 printf "Changing ownership for keystonercv3 file to your user...\n"
-sudo salt "*ctl" cmd.run "sudo chown rbarrett:rbarrett /home/rbarrett/keystonercv3"
+sudo salt "*ctl" cmd.run "sudo chown $USER:$USER /home/$USER/keystonercv3"
 printf "Keystonercv3 has successfully been copied to your home directory and chowned to your user\n"
