2a0a7bfaf8268900e6289021f184f80861c43166
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-08-11 21:41:36 -0500

    Create validate-email-mx.go

---
+++ Go/Snippets/validate-email-mx.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strings"
+)
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func main() {
+	// Made-up domain
+	if e := "test@golangcode-example.com"; !isEmailValid(e) {
+		fmt.Println(e + " is not a valid email")
+	}
+	// Real domain
+	if e := "test@google.com"; !isEmailValid(e) {
+		fmt.Println(e + " not a valid email")
+	}
+}
+
+// isEmailValid checks if the email provided passes the required structure
+// and length test. It also checks the domain has a valid MX record.
+func isEmailValid(e string) bool {
+	if len(e) < 3 && len(e) > 254 {
+		return false
+	}
+	if !emailRegex.MatchString(e) {
+		return false
+	}
+	parts := strings.Split(e, "@")
+	mx, err := net.LookupMX(parts[1])
+	if err != nil || len(mx) == 0 {
+		return false
+	}
+	return true
+}
