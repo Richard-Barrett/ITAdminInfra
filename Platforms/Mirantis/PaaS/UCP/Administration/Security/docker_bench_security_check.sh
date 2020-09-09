7a3566b5335139fd72a2191dd2d0a0251088aa9b
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-09 16:22:58 -0500

    Create docker_bench_security_check.sh

---
+++ Platforms/Mirantis/PaaS/UCP/Administration/Security/docker_bench_security_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check Security Benchmark on Cluster
+echo "============================== SECURITY BENCHMARK CHECK PROCESS ==============================\N"
+while true; do
+    read -p "Do you want to check cluster security (yes/no)?" yn
+    case $yn in
+        [Yy]* ) echo "============================== SECURITY BENCHMARK CHECK IN PROGRESS ==============================\N"; \
+                docker run -it --net host --pid host --cap-add audit_control \
+                -v /var/lib:/var/lib \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -v /usr/lib/systemd:/usr/lib/systemd \
+                -v /etc:/etc --label docker_bench_security \
+                docker/docker-bench-security; \
+		            echo "============================== SECURITY BENCHMARK CHECK IN SUCCESS ===============================\N"; \
+                break;; \
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
