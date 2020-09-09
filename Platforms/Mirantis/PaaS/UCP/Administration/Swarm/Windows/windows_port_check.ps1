e17fd9cd3cb7f281d18be5506c40ad2b6854e379
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-09 16:03:08 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 # Check for Port Issues
-cat ITAdminInfra\Platforms\Mirantis\PaaS\UCP\Adminsitration\Swarm\Windows\tcp_ports.txt | 
+cat ~\ITAdminInfra\Platforms\Mirantis\PaaS\UCP\Adminsitration\Swarm\Windows\tcp_ports.txt | 
 ForEach-Object {Test-NetConnection -ComputerName $ENV:ComputerName -Port $_)
