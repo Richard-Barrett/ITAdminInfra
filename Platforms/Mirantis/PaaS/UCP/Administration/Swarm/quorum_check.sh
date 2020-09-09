c5f378c024c5b4671f56e632aaa19d04eb36ed88
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-09 15:02:04 -0500

    Create quorum_check.sh

---
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/quorum_check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+set -e 
+
+echo "============================== CHECK CLUSTER QUORUM =============================="
+while true; do
+    read -p "Do you wish to check Cluster Swarm Quorum (yes/no)?" yn
+    case $yn in
+        [Yy]* ) echo "==========================\n"; \
+                echo "Checking Cluster Quorum..."; \
+                echo "==========================\n"; \
+                docker node ls -a -f "role=manager"; \
+                echo "==============================\n"; \
+                echo "Checking for Shutdown Tasks..."; \
+                echo "==============================\n"; \
+                docker run -v /var/run/docker/swarm/control.sock:/var/run/swarmd.sock \
+                --entrypoint "./swarmctl" dperny/tasknuke task ls -a | grep SHUTDOWN | wc -l; \
+                echo "==============================\n"; \
+                break;; \
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "============================= CHECK CLUSTER COMPLETE ============================="
+
+while true; do
+    read -p "Do you wish to check Clear Dangling Shutdown Tasks (yes/no)?" yn
+    case $yn in
+        [Yy]* ) echo "===================================\n"; \
+                echo "Clearing Dnagling Shutdown Tasks..."; \
+                echo "===================================\n"; \
+                docker run -v /var/run/docker/swarm/control.sock:/var/run/swarmd.sock \
+                --entrypoint "./swarmctl" dperny/tasknuke task ls -a | grep SHUTDOWN \
+                | awk '{print $1}' | xargs -L 1 sh -c \
+                'docker run -v /var/run/docker/swarm/control.sock:/var/run/swarmd.sock dperny/tasknuke "$0"'
+                echo "===================================\n"; \
+                break;; \
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
