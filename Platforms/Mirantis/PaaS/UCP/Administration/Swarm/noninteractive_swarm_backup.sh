2a39485c792bab673963bbb59834617ef7c8771a
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-11 11:07:40 -0500

    Update noninteractive_swarm_backup.sh

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
@@ -1,3 +1,38 @@
+
+#!/bin/bash
+# ===========================================
+# Created by: Richard Barrett
+# Date Created: 09/11/2020 
+# Purpose: Local or Remote Backup for Swarm 
+# Company: Mirantis
+# ===========================================
+
+# Official documentation
+# ======================================================================================================================
+# https://docs.docker.com/engine/swarm/admin_guide/#back-up-the-swarm
+# https://docs.docker.com/engine/reference/commandline/node_ls/
+# https://www.tutorialspoint.com/unix/if-else-statement.htm
+# https://unix.stackexchange.com/questions/232946/how-to-copy-all-files-from-a-directory-to-a-remote-directory-using-scp
+# https://docs.docker.com/engine/swarm/swarm_manager_locking/
+# https://docs.docker.com/engine/swarm/swarm_manager_locking/#unlock-a-swarm
+# https://linux.die.net/man/1/zip
+# ...
+# ======================================================================================================================
+set -e 
+
+# System Variables 
+MANAGERS=$(docker node ls -f "role=manager")
+
+# Check for auto-lock enabled 
+# If the swarm has auto-lock enabled, you need the unlock key to restore the swarm from backup. 
+# Retrieve the unlock key if necessary and store it in a safe location. 
+# If you are unsure, read Lock your swarm to protect its encryption key.
+
+# Check for Docker Manager Nodes within the Cluster 
+echo "The Following is a list of Managers within your Cluster:"
+echo "=================================== LISTING MANAGERS ===================================\n"
+$MANAGERS 
+echo "=================================== END LISTING MANAGERS ===============================\n"
 # Quorum Check 
 $MANAGERS ; \
 # Make Directory for backup in /tmp/ directory
