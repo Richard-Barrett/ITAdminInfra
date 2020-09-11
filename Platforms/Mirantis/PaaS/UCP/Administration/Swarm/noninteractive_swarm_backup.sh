854b519e5168a24b7af439cf391568bc99370d52
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-11 11:13:18 -0500

    Update noninteractive_swarm_backup.sh

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
@@ -31,21 +31,21 @@ MANAGERS=$(docker node ls -f "role=manager")
 # Check for Docker Manager Nodes within the Cluster 
 echo "The Following is a list of Managers within your Cluster:"
 echo "=================================== LISTING MANAGERS ===================================\n"
-$MANAGERS 
+#$MANAGERS 
 echo "=================================== END LISTING MANAGERS ===============================\n"
 # Quorum Check 
-$MANAGERS ; \
+#$MANAGERS
 # Make Directory for backup in /tmp/ directory
-sudo mkdir /tmp/backup; \
+sudo mkdir /tmp/backup
 # Check if Auto-Lock is Enabled (OPTIONAL)
 # Stop Docker on Local Manager Node for Local Backup
-sudo systemctl stop docker.service; \
+sudo systemctl stop docker.service
 # Store all Files in /tmp/backup/ as /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
-sudo cp -R /var/lib/docker/swarm/ /tmp/backup/swarm_backup_$(date +'%Y%-m%d'); \
+sudo cp -R /var/lib/docker/swarm/ /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
 # Zip backup /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
-which zip; \
-zip -r /tmp/backup/swarm_backup_$(date +'%Y%-m%d').zip /tmp/backup/swarm_backup_$(date +'%Y%-m%d'); \
+which zip
+zip -r /tmp/backup/swarm_backup_$(date +'%Y%-m%d').zip /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
 # Remove Unzipped Contents
-sudp rm -rf /tmp/backup/swarm_backup_$(date +'%Y%-m%d'); \
+sudp rm -rf /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
 # Start Docker Services Locally 
-sudo systemctl start docker.service; \
+sudo systemctl start docker.service
