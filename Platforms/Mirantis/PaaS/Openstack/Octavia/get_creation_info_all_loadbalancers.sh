fd67723b58fccc019a8dff197730cf313a11b0ab
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-14 09:19:55 -0500

    Update get_creation_info_all_loadbalancers.sh

--- Platforms/Mirantis/PaaS/Openstack/Octavia/get_creation_info_all_loadbalancers.sh
+++ Platforms/Mirantis/PaaS/Openstack/Octavia/get_creation_info_all_loadbalancers.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Docuementation 
+# https://serverfault.com/questions/392415/how-to-find-all-filenames-with-given-extension
+# https://docs.openstack.org/python-octaviaclient/latest/cli/index.html
+
+# System Variables 
+CREDS="sudo find / -type f -name 'keystonercv3'"
+
+# Source CREDS
+source $CREDS
+echo "===================================="
+echo "KEYSTONERCV3 CREDS HAVE BEEN SOURCED"
+echo "===================================="
+echo "GETTING ALL INFO CREATED_AT AND ID"
+echo "===================================="
 
 # Get Creation Date Info and ID for all Loadbalancers
 for i in $(openstack loadbalancer list --fit-width | awk '{print $2}' | head -n -1 | \
