69b05490d245cc484067b7f0b1cc8de4f4f3663b
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-15 16:41:28 -0500

    Update noninteractive_swarm_backup.sh

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/noninteractive_swarm_backup.sh
@@ -36,22 +36,43 @@ MANAGERS=$(docker node ls -f "role=manager")
 
 # Check for Docker Manager Nodes within the Cluster 
 echo "The Following is a list of Managers within your Cluster:"
-echo "=================================== LISTING MANAGERS ===================================\n"
+echo "=================================== LISTING MANAGERS ==================================="
 docker node ls -f "role=manager"
-echo "=================================== END LISTING MANAGERS ===============================\n"
+echo "=================================== END LISTING MANAGERS ==============================="
 # Quorum Check 
 # docker node ls -f "role=manager"
 # Make Directory for backup in /tmp/ directory
-sudo mkdir /tmp/backup
+# Look for Backup of The Swarm Directory on System 
+if [[ -d /tmp/backup/ ]] ; then
+    echo 'SWARM BACKUP DIRECTORY EXISTS!'
+fi
+
+if [[ ! -d /tmp/backup/ ]] ; then
+    sudo mkdir /tmp/backup
+    echo 'WARNING SWARM BACKUP DIRECTORY DOES NOT EXIST!'
+    exit
+fi
+
 # Check if Auto-Lock is Enabled (OPTIONAL)
+
 # Stop Docker on Local Manager Node for Local Backup
 sudo systemctl stop docker.service
+echo "DOCKER SERVICE HAS BEEN STOPPED"
+
 # Store all Files in /tmp/backup/ as /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
 sudo cp -R /var/lib/docker/swarm/ /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
+echo "COPIED /var/lib/docker/swarm/ FILES IN /tmp/backup/"
+
 # Zip backup /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
 which zip
 zip -r /tmp/backup/swarm_backup_$(date +'%Y%-m%d').zip /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
+echo "CONTENTS HAVE BEEN ZIPPED"
+
 # Remove Unzipped Contents
 sudo rm -rf /tmp/backup/swarm_backup_$(date +'%Y%-m%d')
+echo "UNZIPPED CONTENTS REMOVED"
+
 # Start Docker Services Locally 
 sudo systemctl start docker.service
+echo "DOCKER SERVICE STARTED"
+echo "BACKUP SUCCESS"
