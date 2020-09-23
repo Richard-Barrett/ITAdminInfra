c23f036397ffc8e4096003c3d2ceebb39837c934
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-23 15:12:57 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -2,6 +2,7 @@
 
 # To see the full list of requirements go to 
 # https://docs.mirantis.com/docker-enterprise/v3.1/dockeree-products/ucp/install-ucp.html#system-requirements
+# https://www.thewindowsclub.com/what-is-a-tcp-and-udp-port-how-to-block-or-open-them-in-windows-10
 
 # Set Current Directory Tree Dynamically
 $CWD = Get-Location | Select-Object -ExpandProperty Path
