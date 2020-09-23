532e1858ed044734ab116a492fbf540c62e665d1
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-23 15:38:51 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -9,13 +9,16 @@ $CWD = Get-Location | Select-Object -ExpandProperty Path
 echo "================================"
 echo "BEGINNING GENERAL TCP PORT CHECK"
 echo "================================"
+echo "...BEGINING CHECK"
 
 # Check for Port Issues
-cat $CWD\tcp_ports.txt | 
-ForEach-Object { if (-not($_ -like '#*')) {Test-NetConnection -ComputerName $ENV:ComputerName -Port $_ } ; 
-                  echo "==========================================" ; 
-                  echo "BEGINNING GENERAL TCP PORT CHECK" ; 
-                  echo "==========================================" ; 
+cat $CWD\tcp_ports.txt | ForEach-Object {
+    if (-not($_ -like '#*')) {
+      echo "==========================================" ;
+      echo "BEGINNING GENERAL TCP PORT $_ CHECK" ;
+      echo "==========================================" ;
+      Test-NetConnection -ComputerName $ENV:ComputerName -Port $_ 
+    }
 }
 
 # Check for Blocked UDP/TCP Ports at ALL Levels 
