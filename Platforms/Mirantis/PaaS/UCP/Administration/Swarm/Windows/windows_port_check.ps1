75afade9f932cfe3ce08ed6657129edf6ac58b39
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-23 14:52:45 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 
 # Set Current Directory Tree Dynamically
-CWD=$(Get-Location | Select-Object -ExpandProperty Path)
+$CWD = Get-Location | Select-Object -ExpandProperty Path
 
 # Check for Port Issues
 cat $CWD\tcp_ports.txt | 
