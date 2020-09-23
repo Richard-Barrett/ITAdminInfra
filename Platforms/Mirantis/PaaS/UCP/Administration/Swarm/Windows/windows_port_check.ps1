463feee8e9cc1ddc2ac2b1ff8534de50d6a6f259
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-23 14:54:35 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -8,4 +8,4 @@ $CWD = Get-Location | Select-Object -ExpandProperty Path
 
 # Check for Port Issues
 cat $CWD\tcp_ports.txt | 
-ForEach-Object {Test-NetConnection -ComputerName $ENV:ComputerName -Port $_)
+ForEach-Object {Test-NetConnection -ComputerName $ENV:ComputerName -Port $_}
