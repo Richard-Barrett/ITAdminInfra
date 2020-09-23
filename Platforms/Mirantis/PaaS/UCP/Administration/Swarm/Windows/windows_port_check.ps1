dd74ac422c56afb252e8b853a5f22787d02e17b2
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-09-23 14:54:15 -0500

    Update windows_port_check.ps1

--- Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
+++ Platforms/Mirantis/PaaS/UCP/Administration/Swarm/Windows/windows_port_check.ps1
@@ -1,5 +1,8 @@
 #!/usr/bin/env pwsh
 
+# To see the full list of requirements go to 
+# https://docs.mirantis.com/docker-enterprise/v3.1/dockeree-products/ucp/install-ucp.html#system-requirements
+
 # Set Current Directory Tree Dynamically
 $CWD = Get-Location | Select-Object -ExpandProperty Path
 
