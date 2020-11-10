37f300cd2377d4a823ebd619bdfbf9eb8ba45819
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-11-10 15:22:00 -0600

    Update mcpinfo.sh
    Initial Commit

--- Platforms/Mirantis/PaaS/MCP/Collection/mcpinfo.sh
+++ Platforms/Mirantis/PaaS/MCP/Collection/mcpinfo.sh
@@ -18,3 +18,41 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+# System Variables 
+# ================
+DATE="$(date +'%Y%-m%d')"
+
+# Check for SOS Report Module
+# Prompt for SOS Report Module Installation
+# Check MCP Version
+
+# Make Directories in /tmp/mcpinfo
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_logs
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_logs/openstack
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_logs/salt
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster/nodes
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster/topology
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster/services
+mkdir /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster/packages
+
+# Get Reclass Topology 
+sudo tree /srv/salt/reclass -J | gzip > /tmp/$(sudo ls /srv/salt/reclass/classes/cluster/)_mcpinfo_$(date +'%Y%-m%d')/$(sudo ls /srv/salt/reclass/classes/cluster/)_cluster/topology/reclass_topology_$(date +'%Y%-m%d').json.gz
+
+# Get Nodes
+# Test-Ping Nodes
+# Get Node DMIDecode Information
+# Get Node Package Information
+# Get Salt Information
+
+# Get Openstack Component Information
+# Get Openstack Version
+# Check for Openstack Related Errors
+
+# Check for System Related Errors
+# Get Service Information
+
+
+
+
