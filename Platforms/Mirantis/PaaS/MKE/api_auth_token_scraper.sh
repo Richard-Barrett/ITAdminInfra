9805cf72eb101f8c15e7b6756c368f4d897083ee
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-12-07 15:58:42 -0600

    Update api_auth_token_scraper.sh

--- Platforms/Mirantis/PaaS/MKE/api_auth_token_scraper.sh
+++ Platforms/Mirantis/PaaS/MKE/api_auth_token_scraper.sh
@@ -2,7 +2,8 @@
 
 USERNAME="admin"
 PASSWORD="dockeradmin"
-AUTH_URL="https://rbarrett-rbarrett-test-rbarrett-ucp-1-ucpleader.train.mirantis.com/auth/login"
+REQUEST_URL="https://rbarrett-rbarrett-test-rbarrett-ucp-1-ucpleader.train.mirantis.com"
+AUTH_URL="${REQUEST_URL}/auth/login"
 
 curl -sk -d "{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"}" $(echo $AUTH_URL) | jq '.' > auth-token
 echo "Bearer auth-token can be found at $(pwd)/auth-token"
