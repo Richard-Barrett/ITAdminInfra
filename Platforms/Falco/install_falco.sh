47622abc0045c6836c6b2cdf3ff4d9fc45e2684f
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2021-01-14 21:41:45 -0600

    Update install_falco.sh

--- Platforms/Falco/install_falco.sh
+++ Platforms/Falco/install_falco.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Documentation:
+# ======================================================
+# https://falco.org/docs/getting-started/installation/
+# ======================================================
+
 curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
 echo "deb https://dl.bintray.com/falcosecurity/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
 apt-get update -y
